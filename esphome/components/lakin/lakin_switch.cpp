7c5ab75e57c35c353c39efe4e6876ea8b832e838
James Lakin <james@jameslakin.co.uk>
2024-07-08 19:25:18 +0000

    Write?

--- esphome/components/lakin/lakin_switch.cpp
+++ esphome/components/lakin/lakin_switch.cpp
@@ -10,10 +10,12 @@ void LakinSwitch::dump_config() { LOG_SWITCH("", "Lakin", this); }
 void LakinSwitch::setup() {
   ESP_LOGCONFIG(TAG, "Setting up Lakin '%s'...", this->name_.c_str());
 
-  
+  pinMode(22, INPUT);
 }
 void LakinSwitch::write_state(bool state) {
   LOG_SWITCH("", state ? "On" : "Off", this);
+
+  digitalWrite(22, state);
 
   this->publish_state(state);
 }
