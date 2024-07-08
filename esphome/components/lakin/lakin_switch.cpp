433e9b260029c0a1ba203ff1ef964adc9ddb7cea
James Lakin <james@jameslakin.co.uk>
2024-07-08 20:31:51 +0000

    Register?

--- esphome/components/lakin/lakin_switch.cpp
+++ esphome/components/lakin/lakin_switch.cpp
@@ -1,21 +1,38 @@
 #include "lakin_switch.h"
+#include <cstdint>
 #include "esphome/core/log.h"
+
+#define REG_GPIO_CFG_BASE_ADDR (0x0802800)
+#define GCFG_OUTPUT_POS 1
+#define GCFG_OUTPUT_BIT (1 << 1)
+#define REG_READ(addr)          (*((volatile uint32_t *)(addr)))
+#define REG_WRITE(addr, _data)  (*((volatile uint32_t *)(addr)) = (_data))
 
 namespace esphome {
 namespace lakin {
 
 static const char *const TAG = "lakin.switch";
 
+static const int PIN_NUM = 22;
+
 void LakinSwitch::dump_config() { LOG_SWITCH("", "Lakin", this); }
 void LakinSwitch::setup() {
   ESP_LOGI(TAG, "Setting up Lakin '%s'...", this->name_.c_str());
 
-  pinMode(22, OUTPUT);
+  pinMode(PIN_NUM, OUTPUT);
 }
 void LakinSwitch::write_state(bool state) {
-  LOG_SWITCH(TAG, "Requested %S", state ? "On" : "Off");
+  ESP_LOGI(TAG, "Requested %s", state ? "On" : "Off");
 
-  digitalWrite(22, state);
+  volatile uint32_t *gpio_cfg_addr = (volatile uint32_t *)(REG_GPIO_CFG_BASE_ADDR + PIN_NUM * 4);
+  ESP_LOGI(TAG, "Address %d", gpio_cfg_addr);
+
+  uint32_t reg_val = REG_READ(gpio_cfg_addr);
+
+  reg_val &= ~GCFG_OUTPUT_BIT;
+  reg_val |= (state ? 1 : 0 & 0x01) << GCFG_OUTPUT_POS;
+  ESP_LOGI(TAG, "Val %d", reg_val);
+  REG_WRITE(gpio_cfg_addr, reg_val);
 
   this->publish_state(state);
 }
