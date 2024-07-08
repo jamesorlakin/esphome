390fdc9abff752f7d0cedd8c28a2697cbabe24ed
James Lakin <james@jameslakin.co.uk>
2024-07-08 19:07:53 +0000

    Add dummy Lakin

---
+++ esphome/components/lakin/lakin_switch.cpp
@@ -0,0 +1,22 @@
+#include "output_switch.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace output {
+
+static const char *const TAG = "lakin.switch";
+
+void LakinSwitch::dump_config() { LOG_SWITCH("", "Lakin", this); }
+void LakinSwitch::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up Lakin '%s'...", this->name_.c_str());
+
+  
+}
+void LakinSwitch::write_state(bool state) {
+  LOG_SWITCH("", state ? "On" : "Off", this);
+
+  this->publish_state(state);
+}
+
+}  // namespace output
+}  // namespace esphome
